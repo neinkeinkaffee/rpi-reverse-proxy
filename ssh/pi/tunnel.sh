70388e9c012acf72c1241f9fe313c7e5e15c0f32
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-05-15 13:22:11 +0200

    Use aws as executable everywhere

--- ssh/pi/tunnel.sh
+++ ssh/pi/tunnel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-EC2_PUBLIC_IP=$(~/.local/bin/aws ec2 describe-instances --filter "Name=tag:Name,Values=proxy" \
+EC2_PUBLIC_IP=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=proxy" \
 --query "Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp" \
 --no-paginate | jq -r '.[0]')
 autossh -fNC -R 10011:localhost:22 ec2-user@$EC2_PUBLIC_IP
