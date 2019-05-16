6f45adb469e9a8b31b3cc8ba341278a16d496c50
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-05-16 21:30:25 +0200

    Move ip updates into separate scripts

---
+++ ssh/local/connect-pi.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Which of your Raspberry Pis do you want to connect to?"
+echo "0 pi0"
+echo "1 pi1"
+echo "2 pi2"
+read PI
+
+EC2_PUBLIC_IP=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=proxy" \
+--query "Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp" \
+--no-paginate | jq -r '.[0]')
+ssh pi@${EC2_PUBLIC_IP} -p 1${PI}${PI}11
--- ssh/local/update-ip.sh
+++ ssh/local/update-ip.sh
@@ -17,14 +17,25 @@ done
 # Find currently allowed IP ranges (variable interpolation in jq isn't working as documented)
 CIDRS=$(aws ec2 describe-security-groups --group-ids $SG \
     | jq -r '.SecurityGroups[].IpPermissions[]
-    | select(.FromPort == 22 and .ToPort == 22) | .IpRanges[].CidrIp')
+    | select(.FromPort == 2222 and .ToPort == 2222) | .IpRanges[].CidrIp')
 
 # Revoke access for currently allowed IP ranges
 for ip in $CIDRS; do
 	[ "$MYIP/32" != "$ip" ] && aws ec2 revoke-security-group-ingress \
-		--group-id $SG --protocol tcp --port 22 --cidr $ip
+		--group-id $SG --protocol tcp --port 2222 --cidr $ip
 done
 
 # Allow access for IP of this machine
 [ -z $(echo "$CIDRS" | grep "$MYIP/32") ] && aws ec2 authorize-security-group-ingress \
-    --group-id $SG --protocol tcp --port 22 --cidr "$MYIP/32"
+    --group-id $SG --protocol tcp --port 2222 --cidr "$MYIP/32"
+
+echo "Which of your Raspberry Pis do you want to connect to?"
+echo "0 pi0"
+echo "1 pi1"
+echo "2 pi2"
+read PI
+
+EC2_PUBLIC_IP=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=proxy" \
+--query "Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp" \
+--no-paginate | jq -r '.[0]')
+ssh pi@${EC2_PUBLIC_IP} ssh -p 1${PI}${PI}11
