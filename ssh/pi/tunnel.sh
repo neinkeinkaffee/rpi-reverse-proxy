9cb8396b6717734e00261097071c2bf01adc15b2
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-05-19 17:57:32 +0200

    Fix autossh and ssh commands

--- ssh/pi/tunnel.sh
+++ ssh/pi/tunnel.sh
@@ -4,4 +4,4 @@ set -x
 EC2_PUBLIC_IP=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=proxy" \
 --query "Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp" \
 --no-paginate | jq -r '.[0]')
-autossh -fNC -R 10011:localhost:22 8000:127.0.0.1:8000 ec2-user@$EC2_PUBLIC_IP
+autossh -fNC -R 10011:localhost:22 -R 8000:127.0.0.1:8000 ec2-user@$EC2_PUBLIC_IP
