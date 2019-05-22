c27fde1ed36bd7540b4a6ad5f3e5d3ff2ac9e4cd
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-05-22 20:48:50 +0200

    Remove connect to rpi part

--- ssh/local/update-ip.sh
+++ ssh/local/update-ip.sh
@@ -28,14 +28,3 @@ done
 # Allow access for IP of this machine
 [ -z $(echo "$CIDRS" | grep "$MYIP/32") ] && aws ec2 authorize-security-group-ingress \
     --group-id $SG --protocol tcp --port 2222 --cidr "$MYIP/32"
-
-echo "Which of your Raspberry Pis do you want to connect to?"
-echo "0 pi0"
-echo "1 pi1"
-echo "2 pi2"
-read PI
-
-EC2_PUBLIC_IP=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=proxy" \
---query "Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp" \
---no-paginate | jq -r '.[0]')
-ssh -t -i ~/.ssh/aws-free-tier ec2-user@${EC2_PUBLIC_IP} ssh -p 1${PI}${PI}11 pi@localhost
