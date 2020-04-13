5dcd34f332093b6fd0f44362da9ce1e1060f18eb
Gesa Stupperich <gesa.stupperich@gmail.com>
2020-04-13 13:54:01 +0200

    Modify laptop installation script to work on OSX

--- tinc/install-tinc-laptop.sh
+++ tinc/install-tinc-laptop.sh
@@ -15,7 +15,7 @@ brew install tinc
 sudo mkdir -p /etc/tinc/rpinet/hosts
 printf "Name = $LAPTOP_NODE_NAME\nAddressFamily = ipv4\nInterface = tun0\nConnectTo = ec2" | sudo tee /etc/tinc/rpinet/tinc.conf
 printf "Subnet = 10.0.0.$NODE_LAST_OCTET/32" | sudo tee /etc/tinc/rpinet/hosts/$LAPTOP_NODE_NAME
-printf '#!/bin/sh\nifconfig $INTERFACE '"10.0.0.$LAPTOP_NODE_LAST_OCTET netmask 255.255.255.0" | sudo tee /etc/tinc/rpinet/tinc-up
+printf '#!/bin/sh\nifconfig tun0 inet '"10.0.0.$LAPTOP_NODE_LAST_OCTET 10.0.0.1 netmask 255.255.255.0" | sudo tee /etc/tinc/rpinet/tinc-up
 printf '#!/bin/sh\nifconfig $INTERFACE down' | sudo tee /etc/tinc/rpinet/tinc-down
 sudo chmod 755 /etc/tinc/rpinet/tinc-*
 sudo tincd -c /etc/tinc/rpinet --pidfile=/var/run/tincd.pid -n rpinet -K4096
